8b6024021e3903833cc854b77d1d08189149ad9a
Yury Zaytsev <more.jorand@gmail.com>
2017-07-14 13:25:29 +0200

    Final reault

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 $(function(){
 	$(document).ready(function(){
-	  	$(".owl-carousel").owlCarousel();
+	  	$("#owl-demo").owlCarousel({		 
+		    slideSpeed : 300,
+		    paginationSpeed : 400,
+		 
+		    animateOut: 'slideOutDown',
+		    animateIn: 'flipInX',
+		    items:1,
+		    loop: true,
+		    stagePadding:30,
+    		smartSpeed:450,
+    		itemsDesktop : false,
+		    itemsDesktopSmall : false,
+		    itemsTablet: false,
+		    itemsMobile : false,
+		    margin:30
+		 
+		});
+
+			$('.overlay').click(function() {
+			$(this).remove();
+		});
 	});
+});
+
+$(window).load(function () {
+    var imgWidht = $('.team img').width();
+    $('.team img').each(function () {
+       $(this).attr('width', imgWidht);
+    });
+    $('.team .portfolio').css('max-width', imgWidht + 'px');
 });
